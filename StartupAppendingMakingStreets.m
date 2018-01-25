49f2257b8875e2d8216af4f8e5eafdaddd7079d8
Guy Nir <guy_nir@hms.harvard.edu>
2018-01-25 17:29:31 -0500

    Add files via upload

---
+++ StartupAppendingMakingStreets.m
@@ -0,0 +1,9 @@
+%% Startup for Appending and MakingStreets tools
+% -------------------------------------------------------------------------
+%% Call streets
+addpath(genpath('/Users/guynir/Documents/MATLAB/Appending/'));
+%% Define env
+setenv('PATH','/usr/bin:/usr/local/bin:/usr/local/bin/bin');
+setenv('NUPACKHOME','/Users/guynir/nupack3.0.6');
+!echo $PATH
+!echo $NUPACKHOME
